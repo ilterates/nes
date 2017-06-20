ac27f21c65e1f77971d2329a45d921f9cd297444
ilterates <iltates@gmail.com>
2017-06-19 21:23:53 -0700

    loading js

---
+++ public/js.js
@@ -0,0 +1,15 @@
+var hidden = false;
+window.setInterval( function() {
+  if (hidden === false) {
+    $("#moving").animate({left: "-20px"})
+                .animate({bottom: "20px"})
+                .fadeOut( 500 );
+     setTimeout(500);
+     hidden = true;
+ } else {
+  $("#moving").animate({left: "20px"})
+              .animate({bottom: "-20px"})
+              .fadeIn( 500 );
+   hidden = false;
+ }
+}, 1000);
