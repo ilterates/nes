a6c1f14e6a33403b84181e862c28838c502615d7
ilterates <iltates@gmail.com>
2017-06-29 01:47:55 -0700

    menu

--- public/js/js.js
+++ public/js/js.js
@@ -1,8 +1,14 @@
 console.log("Nesly!");
-
+function hidden() {
+  $( "#nav" ).hide();
+}
+hidden();
 function welcomeFadeOut() {
   $( "#welcome-text" ).fadeOut( "slow", function() {
       // Animation complete.
+  });
+  $( "#nav" ).show( "slow", function() {
+
   });
 }
 
